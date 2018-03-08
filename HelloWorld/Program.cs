d81f06b7210b88bc8baee28f3932a514acc9ed22
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-03-08 02:30:50 -0500

    finished first page of guide

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -46,6 +46,38 @@ namespace HelloWorld
             Console.WriteLine(DoubleInterchange);
             Console.WriteLine(Charizard);
             Console.WriteLine(StringCheese);
+
+            int Overflow = int.MaxValue;
+            Console.WriteLine(Overflow);
+            Overflow += 1;
+            Console.WriteLine(Overflow == int.MinValue);
+            // When we increment the max value, it wraps around to the min value
+
+            var ImplicitBool = true;
+            var ImplicitInt = 5;
+            var ImplicitDouble = 25.52;
+            var ImplicitChar = 'X';
+            var ImplicitString = "X";
+
+            int x = 2;
+            int y = 5;
+
+            int z1 = x++;
+            int z2 = ++y;
+
+            Console.WriteLine(x);
+            // X = 3 now
+            Console.WriteLine(y);
+            // Y = 6 now
+            Console.WriteLine(z1);
+            // Z1 uses the postfix operator and holds the pre-incremented value of X
+            Console.WriteLine(z2);
+            // Z2 uses the prefix operator and holds the post-incremented value of Y
+
+            int z = 10;
+            int a = z * x - y;
+            int b = y - x % z;
+            int c = y/z + x;
         }
     }
 }
